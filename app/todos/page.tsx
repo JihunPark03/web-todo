4ec0eb46f02cf744113afa9fe8699fa209b7b795
Jihun Park <mytwinbaby37@gmail.com>
2024-05-21 15:21:21 +0900

    change

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -4,16 +4,23 @@ import TodosTable from "@/components/todos-table";
 async function fetchTodosApiCall(){
 	console.log("fetchTodoApi called");
 	const resposne = await fetch(`${process.env.BASE_URL}/api/todos`,{cache: 'no-store'})
+	const contentTypeCheck = resposne.headers.get('Content-Type');
+	if(contentTypeCheck?.includes("text/html")){
+		return null;
+	}
 	return resposne.json();
 }
 
 export default async function Todospage() {
 
 	const response = await fetchTodosApiCall();
+
+	const fetchedTodos= response?.data ?? [];
+
 	return (
 		<div className="flex flex-col space-y-8">
 			<h1 className={title()}>Todos</h1>
-			<TodosTable todos={response.data ?? []}/>
+			<TodosTable todos={fetchedTodos}/>
 		</div>
 	);
 }
